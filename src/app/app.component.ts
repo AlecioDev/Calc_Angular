8d558860754b10c1bc330888b73544fa74bc56f6
Felipe Geteski Almeida <83322191+fegeteski@users.noreply.github.com>
2022-09-19 23:58:27 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'CalculadoraAngular';
+  result : number=0;
+  num1 : number=0;
+  num2 : number=0;
+
+  somar(){
+    this.result=this.num1+this.num2;
+  }
+  sub(){
+    this.result=this.num1-this.num2;
+  }
+  multi(){
+    this.result=this.num1*this.num2;
+  }
+  divis(){
+    this.result/=this.num1+this.num2;
+  }
+  raiz(){
+    this.result=Math.sqrt(this.num1+this.num2);
+  }
+  zerar(){
+    this.result=0;
+  }
+
 }
+
