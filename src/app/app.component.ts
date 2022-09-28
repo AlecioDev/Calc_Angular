7df68c339e8d682f1f2b8ebd5ccaeeebbc68e380
Felipe Geteski Almeida <83322191+fegeteski@users.noreply.github.com>
2022-09-28 20:26:37 -0300

    First commit to Angular Calculator!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,28 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'CalculadoraAngular';
-  result : number=0;
-  num1 : number=0;
-  num2 : number=0;
+  result? : number;
+  num1? : number;
+  num2? : number;
 
   somar(){
-    this.result=this.num1+this.num2;
+    this.result=this.num1!+this.num2!;
   }
   sub(){
-    this.result=this.num1-this.num2;
+    this.result=this.num1!-this.num2!;
   }
   multi(){
-    this.result=this.num1*this.num2;
+    this.result=this.num1!*this.num2!;
   }
   divis(){
-    this.result/=this.num1+this.num2;
+    this.result! /=this.num1!+this.num2!;
   }
   raiz(){
-    this.result=Math.sqrt(this.num1+this.num2);
+    this.result=Math.sqrt(this.num1!+this.num2!);
   }
   zerar(){
     this.result=0;
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {LayoutModule} from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -12,6 +18,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    LayoutModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
